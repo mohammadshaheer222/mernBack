725f0081237a376599a6c3d0c6ea7288f1603d7d
mohammad shaheer <shaheercse2022@gmail.com>
2024-04-01 23:42:48 +0530

    change connect file

--- Db/connectDb.js
+++ Db/connectDb.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDb = (url) => {
-  mongoose.connect(url);
+  mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 };
 
 module.exports = {connectDb};
