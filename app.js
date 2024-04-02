b6e4f00405540be57e9aae21b22867d8e9ea1983
mohammad shaheer <shaheercse2022@gmail.com>
2024-04-02 22:48:41 +0530

    change frontend api

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const wishListRouter = require("./routes/wishListRouter");
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://mernfront-acyh.onrender.com/", "http://localhost:5174"],
+    origin: ["https://frontend-uvib.onrender.com", "http://localhost:5174"],
     credentials: true,
   })
 );
